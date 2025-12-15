bf0ae8077f976274a8fbce077d6c7c907c7c6985
Phuong Thao Phan <phuongthao@Phuongs-Air.fritz.box>
2025-12-15 20:12:59 +0100

    Update package.json and vite config.

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({ 
   // Set base to the repository name only. 
   // This is the standard setting for GitHub Pages.
-  base: process.
+  base: process.env.NODE_ENV === 'production' 
+        ? '/CCLab_AutobiographicalGame/' 
+        : '/',
+  build: {
+    // This setting tells Vite to keep assets close to the index.html file
+    assetsDir: '.', 
+  }
+});
