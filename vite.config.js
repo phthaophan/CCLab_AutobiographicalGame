0cd46f7ef377bd4ed47e1397bc6b8a991e2a811b
Phuong Thao Phan <phuongthao@Phuongs-Air.fritz.box>
2025-12-15 19:47:57 +0100

    sound path changed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 
-
 export default defineConfig({ 
+  // Set base to the repository name only. 
+  // This is the standard setting for GitHub Pages.
   base: process.env.NODE_ENV === 'production' 
-        ? '/CCLab_AutobiographicalGame/dist/'
+        ? '/CCLab_AutobiographicalGame/' 
         : '/',
 });
