7b0b28f08e41c5c9870732afa7c78219739ecd89
Phuong Thao Phan <phuongthao@Phuongs-Air.fritz.box>
2025-12-15 20:08:29 +0100

    fixed texture path

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,4 @@ import { defineConfig } from 'vite';
 export default defineConfig({ 
   // Set base to the repository name only. 
   // This is the standard setting for GitHub Pages.
-  base: process.env.NODE_ENV === 'production' 
-        ? '/CCLab_AutobiographicalGame/' 
-        : '/',
-  build: {
-    // This setting tells Vite to keep assets close to the index.html file
-    assetsDir: '.', 
-  }
-});
+  base: process.
