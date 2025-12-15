3fe19d86205e1fc1540a4113797facb8dad30ea5
Phuong Thao Phan <phuongthao@Phuongs-Air.fritz.box>
2025-12-15 19:37:07 +0100

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({ 
   base: process.env.NODE_ENV === 'production' 
-        ? '/CCLab_AutobiographicalGame/'
+        ? '/CCLab_AutobiographicalGame/dist/'
         : '/',
 });
