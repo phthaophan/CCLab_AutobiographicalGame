55c886ac34a93c394f9db635f6007a3367354b60
phthaophan <hi@phuongthaophan.design>
2026-01-26 11:32:53 -0500

    fix base path after repo rename

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   // Set base to the repository name only. 
   // This is the standard setting for GitHub Pages.
   base: process.env.NODE_ENV === 'production' 
-        ? '/CCLab_AutobiographicalGame/' 
+        ? '/ChangeofPerspective/' 
         : '/',
   build: {
     // This setting tells Vite to keep assets close to the index.html file
